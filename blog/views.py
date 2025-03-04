6577261f744f1ba396a7f9623cc526a4255b4c72
SonyaJane <sonya.ridden@gmail.com>
2025-03-04 14:33:53 +0000

    Enable user to submit comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -21,7 +23,18 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment submitted and awaiting approval')
 
+
+    comment_form = CommentForm()
+    
     # render puts together the template and the context dictionary and returns an HttpResponse object with that rendered text.
     # The context is a dictionary mapping template variable names to Python objects.
     # When rendering a template, Django replaces variables in the template with values from the context dictionary.
@@ -32,5 +45,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         }, # things we want to pass to the template, in the form of a dictionary
     )
