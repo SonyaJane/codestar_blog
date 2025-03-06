837f0a4dcdfcf00ce5994a3ced5ba0e0ae36c06e
SonyaJane <sonya.ridden@gmail.com>
2025-03-06 18:48:10 +0000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     # assigns a view called post_detail to the slug URL. name='post_detail' is the name of the URL that will be used to identify the view
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    # assigns a view called comment_edit to the slug URL. name='comment_edit' is the name of the URL that will be used to identify the view
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
+
 ]
